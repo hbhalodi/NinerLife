9a5999b670c30631fcb8edfffe93f13f3221015d
hasti <hbhalodi@charlotte.edu>
2026-09-06 16:25:09 -0400

    Add workload calculation functions

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# -------------------------
+# FUNCTIONS
+# -------------------------
+
+def calculate_workload(classes,assignments,exams,work_hours):
+    score = (
+    (classes*5)+
+    (assignments*5)+
+    (exams*10)+
+    (work_hours)
+)
+    return score
+
+def get_wokrload_level(score):
+    if score <40:
+        return "Low"
+    elif score <70:
+        return "Medium"
+    else:
+        return "High"
+
+
+# -------------------------
+# MAIN PROGRAM
+# -------------------------
+
 print("================================")
 print("       Welcome to NinerLife")
 print("================================")
@@ -8,18 +34,10 @@ work_hours = float(input("How many hours do you work per week? "))
 assignments = int(input("How many assignments do you have this week? "))
 exams= int(input("How many emaxams do you have this week ? "))
 
-workload_score=(
-    (classes*5)+(assignments*5)+(exams*10)+(work_hours)
-)
+workload_score= calculate_workload(classes,assignments,exams,work_hours)
 
-if workload_score<40:
-    workload_level ="Low"
 
-elif workload_score<70:
-    workload_level ="Medium"
-
-else:
-    workload_level="High"
+level=get_wokrload_level(workload_score)
 
 print("\n================================")
 print("          YOUR RESULTS")
@@ -34,12 +52,12 @@ print("Exams:", exams)
 
 
 print("\nYour workload score is:", workload_score)
-print("Workload Level:", workload_level)
+print("Workload Level:",level)
 
-if workload_level == "Low":
+if level == "Low":
     print("You have a manageable week. Keep up the good work!")
 
-elif workload_level == "Medium":
+elif level == "Medium":
     print("Your week looks somewhat busy. Make sure you plan your time.")
 
 else:
